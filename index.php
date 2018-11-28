c68bf8a0446f4db9e2299590964ef5a2dbed298d
oroszlaszlolaci <39948459+oroszlaszlolaci@users.noreply.github.com>
2018-11-28 12:06:38 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TÁRS-95</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+<meta charset="utf-8">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css">
+  <link href="https://fonts.googleapis.com/css?family=Raleway%7CRubik:400" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Great+Vibes%7CMuli:400,700%7CLibre+Baskerville:300,400,700" rel="stylesheet">
+</head>
+<body>
+	<header>	
+		<div class="logo">
+		<h1>Üdvözöljük a Társ-95-nél</h1>
+		</div>
+
+	</header>
+<div id="nav">
+	</div>
+  <ul>
+  	<li><a href="#header">Kezdő</a></li>
+  	<li><a href="#company">Bemutatkozás</a></li>
+  	<li><a href="#machines">Géppark</a></li>
+  	<li><a href="#activity">Tevékenység</a></li>
+  	<li><a href="#reference">Referenciák</a></li>
+  	<li><a href="#certifications">Tanusítványok</a></li>
+  	<li><a href="#contact">Kapcsolat</a></li>
+</ul>
+
+	<div id="company">
+		<div id="content">
+			<h1>BEMUTATKOZÁS</h1>
+			<br>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam urna nunc, facilisis ac vehicula in, bibendum id orci. Etiam finibus, nunc malesuada sagittis eleifend, enim ante luctus ligula, in faucibus lectus augue efficitur ante. Mauris in sem sollicitudin, dictum lorem tincidunt, viverra ligula. Duis mi libero, iaculis sed libero quis, efficitur eleifend leo. Vestibulum sit amet nibh vitae lectus elementum elementum. Curabitur sed viverra quam, ac ullamcorper elit. Praesent sapien nisl, tincidunt molestie bibendum quis, vehicula vel ligula. Mauris tortor erat, vehicula eget venenatis nec, placerat eget est.</p>
+			<br>
+			<p>Fusce at neque risus. Integer ultrices felis in egestas sollicitudin. Integer iaculis auctor ipsum ut porta. In faucibus a magna a rutrum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed sagittis libero ligula, a pretium lectus imperdiet et. Nulla pellentesque non elit non bibendum. Integer et dui rutrum, sollicitudin dolor vitae, volutpat lacus. Ut justo nisl, aliquam ut augue eget, porta molestie purus. Donec tortor tellus, scelerisque fermentum ante eu, tincidunt fringilla risus</p>
+			<br>
+			<p>Ut varius sagittis ligula, ut ullamcorper tortor pretium quis. Duis eros risus, tincidunt ut vehicula et, gravida et sapien. Phasellus ipsum magna, accumsan in turpis quis, aliquam consequat diam. Nam pulvinar est et.</p>
+			<br>
+			<p>Fusce at neque risus. Integer ultrices felis in egestas sollicitudin. Integer iaculis auctor ipsum ut porta. In faucibus a magna a rutrum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed sagittis libero ligula, a pretium lectus imperdiet et. Nulla pellentesque non elit non bibendum. Integer et dui rutrum, sollicitudin dolor vitae, volutpat lacus. Ut justo nisl, aliquam ut augue eget, porta molestie purus. Donec tortor tellus, scelerisque fermentum ante eu, tincidunt fringilla risus</p>
+		</div>
+	</div>
+	
+
+	<div id="machines">
+		<p>GÉPEK</p>
+
+	</div>
+	<div id="activity">
+		<h1 class="heading">TEVÉKENYSÉG</h1>
+		<div id="table">
+			<h2>Acélszerkezet gyártás</h2>
+			<h2>Generálkivitelezés</h2>
+			<h2>Kereskedelmi és szolgáltató tevékenység</h2>
+
+		</div>
+
+
+	</div>
+
+	<div id="references">
+		<h1>REFERENCIÁK</h1>
+	</div>
+
+	
+
+	<div id="certifications">
+		<p>TANÚSÍTVÁNYOK</p>
+
+	</div>
+
+	
+
+
+
+	<div id="partners">
+		<p>PARTNEREK LOGÓJA</p>
+
+	</div>
+
+
+	<div id="contact">
+		<h2>KAPCSOLAT</h2>
+		<div id="address">
+			
+		</div>
+
+		<div id="mail">
+			
+		</div>
+
+	</div>
+
+	
+
+
+
+
+
+
+	<footer>
+		<div class="footer-wrapper">
+			&copy; <?php echo date("Y"); ?> TÁRS-95 KFT. | Minden jog fenntartva.
+		</div>
+		</footer>
+
+
+</body>
+</html>
+
+
+
+
+<!--colors_ fresh-#F7F5E6 VERMILLION-#333A56  SUNSHINE-#52658F  CLEAN-#E8E8E8   -->
